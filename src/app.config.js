b5f1a82e5f46f0e9679ac6e1255a3d92fafe97d9
Shinra DX <shinra.dx@outlook.com>
2021-11-15 22:25:21 +0800

    Add config file

---
+++ src/app.config.js
@@ -0,0 +1,7 @@
+export default {
+    // 此处填写查分器的用户名。如未注册查分器，
+    // 请前往 https://https://www.diving-fish.com/maimaidx/prober/ 注册。
+    // 注册账号后，请按照网站的使用指南导入数据。
+    // 请注意，使用该程序前，确保您的查分器账号禁用“不允许他人查看成绩”选项。
+    userId: "your_user_id"
+}
